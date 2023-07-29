68501d637c9fd744276c4ca5ad94dacfa4460b16
Ethan Alker <ealker1@gmail.com>
2023-07-29 18:28:33 -0400

    remove scopes arg from init

--- fishify-lib/src/lib.rs
+++ fishify-lib/src/lib.rs
@@ -3,15 +3,16 @@ pub mod spotify;
 
 use config::ClientConfig;
 
-use std::collections::HashSet;
 use anyhow::Result;
 use rspotify::{
     AuthCodeSpotify, Credentials, OAuth, Config,
+    scopes,
     clients::{ OAuthClient, BaseClient, },
 };
 
 // init with sensible defaults, if you want more control do it manually
-pub async fn spotify_init(scopes: HashSet<String>) -> Result<AuthCodeSpotify> {
+// AuthCodeSpotify must be used, OAuthClient is Sized and therefore cannot be a trait object, so i gotta use a real type
+pub async fn spotify_init() -> Result<AuthCodeSpotify> {
     let mut client_config = ClientConfig::new();
     client_config.load_config()?;
 
@@ -20,7 +21,10 @@ pub async fn spotify_init(scopes: HashSet<String>) -> Result<AuthCodeSpotify> {
     let creds = Credentials::new(&client_config.client_id, &client_config.client_secret);
     let oauth = OAuth {
         redirect_uri: client_config.get_redirect_uri(),
-        scopes: scopes,
+        scopes: scopes!(
+            "user-modify-playback-state", 
+            "user-read-playback-state"
+        ),
         ..Default::default()
     };
     let config = Config {
